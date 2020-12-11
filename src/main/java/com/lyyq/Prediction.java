54df7890d6773d4cff369832391f63ba49374d8a
threethousanddrops <62058640+threethousanddrops@users.noreply.github.com>
2020-12-11 20:01:57 +0800

    Update Prediction.java

--- src/main/java/com/lyyq/Prediction.java
+++ src/main/java/com/lyyq/Prediction.java
@@ -1,98 +1,101 @@
 package com.lyyq;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.*;
 
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Prediction {
-    public class WordCountMapper 
-            extends Mapper<LongWritable,Text,Text,IntWritable>{
-        Text item = new Text();
-        IntWritable v = new IntWritable(1);
-        @Override
-        protected void map(LongWritable key, Text value, Context context) 
-                throws IOException, InterruptedException {
-            String line = value.toString();
-            String[] fields = line.split(",");
-            if(fields[6].equals("1")||fields[6].equals("2")||fields[6].equals("3")){
-                item.set(fields[1]);
-                context.write(item, v);
-            } 
+
+public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
+
+    private Text word = new Text();
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] fields = line.split(",");
+        if(fields[6].equals("1")||fields[6].equals("2")||fields[6].equals("3")){
+            this.word.set(fields[1]);
+            context.write(this.word, new Text("1"));
         }
     }
+}
 
+public static class IntSumReducer extends Reducer<Text, Text, Text, Text> {
+   
+  private IntWritable result = new IntWritable();
+    private Text word = new Text();
 
-    public class WordCountReducer 
-            extends Reducer<Text,IntWritable,Text,IntWritable>{
-        IntWritable v = new IntWritable();
-        Map<String,Integer> map=new HashMap<String,Integer>();
+    private static TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>(new Comparator<Integer>() {
         @Override
-        protected void reduce(Text key, Iterable<IntWritable> values, Context context) 
-                throws IOException, InterruptedException {
-            int sum=0;
-            for (IntWritable value : values) {
-                sum+=value.get();
-            }
- 
-            String k=key.toString();
-            map.put(k, sum);
+        public int compare(Integer x, Integer y) {
+            return y.compareTo(x);
         }
+    });
+
+    public void reduce(Text key, Iterable<Text> values, Context context)
+               throws IOException, InterruptedException{
+          int sum = 0;
+          for (Text val: values)
+          {
+              sum+=Integer.valueOf(val.toString());
+          }
+          treeMap.put(new Integer(sum), key.toString());
+      }
+
+    protected void cleanup(Context context)
+        throws IOException,InterruptedException{
+        Set<Map.Entry<Integer, String>> set = treeMap.entrySet();
+        int count =1;
+        for (Map.Entry<Integer, String> entry : set) {
+            this.result.set(entry.getKey());
+            this.word.set(count+":"+entry.getValue()+", "+entry.getKey());
+            context.write(word, new Text(""));
+            count++;
+            if(count>100){
+              return;
+            }
+        }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+
+    GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
+    String[] remainingArgs = optionParser.getRemainingArgs();
+    if (remainingArgs.length !=3) {
+      System.err.println("Usage: wordcount <in> <out> [-skip]");
+      System.exit(2);
+    } 
+
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(Prediction.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
     
-        @Override
-        protected void cleanup(Context context) throws IOException, InterruptedException {
-            List<Map.Entry<String,Integer>> list=new LinkedList<Map.Entry<String,Integer>>(map.entrySet());
-            Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
-                @Override
-                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                    return (int)(o2.getValue()-o1.getValue());
-                }
-            });
-            
-            for(int i=0;i<5;i++){
-                context.write(new Text(list.get(i).getKey()), new IntWritable(list.get(i).getValue()));
-            }
-        }    
+    Path outPath=new Path(args[1]);
+    FileSystem fstm = FileSystem.get(conf);
+    if (fstm.exists(outPath)) {
+        fstm.delete(outPath, true);
     }
 
-    public static void main(String[] args) 
-        throws IOException, InterruptedException, ClassNotFoundException{
-        Configuration conf = new Configuration();
-        GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
-        String[] remainingArgs = optionParser.getRemainingArgs();
-            if (remainingArgs.length !=2) {
-                System.err.println("Usage: wordcount <in> <out> ");
-                System.exit(2);
-        } 
-
-        Job job = Job.getInstance(conf);
-        job.setJarByClass(Prediction.class);
-        job.setMapperClass(WordCountMapper.class);
-        job.setReducerClass(WordCountReducer.class);
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-
-        Path outPath=new Path(args[1]);
-        FileSystem fstm = FileSystem.get(conf);
-        if (fstm.exists(outPath)) {
-            fstm.delete(outPath, true);
-        }
-        
-        FileInputFormat.setInputPaths(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, outPath);
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, outPath);
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
