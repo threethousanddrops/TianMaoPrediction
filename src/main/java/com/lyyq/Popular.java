90f5c52aff1ced17d73d8c2029898a8587f0607b
threethousanddrops <62058640+threethousanddrops@users.noreply.github.com>
2020-12-11 22:34:08 +0800

    Update and rename Prediction.java to Popular.java

--- src/main/java/com/lyyq/Popular.java
+++ src/main/java/com/lyyq/Popular.java
@@ -1,6 +1,8 @@
 package com.lyyq;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
@@ -15,26 +17,41 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class Prediction {
+public class Popular {
 
 public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
 
     private Text word = new Text();
+    private Set<String> patternsToskip=new HashSet<>();
+    protected void setup(Context context) throws IOException, InterruptedException{
+        FileSystem fs = FileSystem.get(context.getConfiguration());
+
+        Path path1 = new Path("hdfs://lyyq181850099-master:9000/task/user_info_skip.csv");
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(fs.open(path1)));
+        String liner;
+        while ((liner = reader1.readLine()) != null) {
+          patternsToskip.add(liner.toLowerCase());
+        }
+        reader1.close();
+    }
+
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         String[] fields = line.split(",");
         if(fields[5].equals("1111")){
+            if(!(this.patternsToskip.contains(fields[0]))){
                 if(fields[6].equals("1")||fields[6].equals("2")||fields[6].equals("3")){
                 this.word.set(fields[1]);
                 context.write(this.word, new Text("1"));
+                }
             }
-        }      
+        }
     }
 }
 
 public static class IntSumReducer extends Reducer<Text, Text, Text, Text> {
    
-  private IntWritable result = new IntWritable();
+    private IntWritable result = new IntWritable();
     private Text word = new Text();
 
     private static TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>(new Comparator<Integer>() {
@@ -81,12 +98,12 @@ public static class IntSumReducer extends Reducer<Text, Text, Text, Text> {
     } 
 
     Job job = Job.getInstance(conf, "word count");
-    job.setJarByClass(Prediction.class);
+    job.setJarByClass(Popular.class);
     job.setMapperClass(TokenizerMapper.class);
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(Text.class);
-    
+
     Path outPath=new Path(args[1]);
     FileSystem fstm = FileSystem.get(conf);
     if (fstm.exists(outPath)) {
